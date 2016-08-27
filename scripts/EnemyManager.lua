bd2a4247b26b84409d635a2604b93dad858b2e46
Coac <levictor3000@hotmail.fr>
2016-08-27 22:44:09 +0200

    Random spawning enemy

--- scripts/EnemyManager.lua
+++ scripts/EnemyManager.lua
@@ -3,9 +3,11 @@ require("scripts/globals")
 
 EnemyManager = {}
 
+local enemyImage
 function EnemyManager:new()
-    local enemyImage = lg.newImage(imagesFolder .. "enemy.png")
+  enemyImage = lg.newImage(imagesFolder .. "enemy.png")
   local o = {
+    time = 0,
     enemies = {
       Enemy:new(enemyImage, 300, 300, 10, 2),
       Enemy:new(enemyImage, 100, 100, 10, 2)
@@ -40,6 +42,14 @@ function EnemyManager:update(dt)
   for i, enemy in ipairs(self.enemies) do
     enemy:update(dt)
   end
+
+  self.time = self.time + dt
+  if(self.time > 2) then
+    self.time = 0
+    local halfWidth = width_window / 2
+    self:add(Enemy:new(enemyImage, halfWidth + math.rsign()*halfWidth, math.random(0, height_window), 2))
+  end
+
 end
 
 function EnemyManager:draw()
