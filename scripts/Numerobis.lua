3b78e1be9e557d04dd2bb04a04ea027a15679776
Coac <levictor3000@hotmail.fr>
2016-08-27 15:12:54 +0200

    Numerobis move

---
+++ scripts/Numerobis.lua
@@ -0,0 +1,39 @@
+local lg = love.graphics
+local lk = love.keyboard
+require("scripts/globals")
+
+Numerobis = {}
+
+function Numerobis:new(_x, _y, _speed)
+  o = {
+    sprite = lg.newImage(imagesFolder .. "numerobis.png"),
+    x = _x,
+    y = _y,
+    speed =  _speed
+   }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+function Numerobis:update(dt)
+  if lk.isDown("right") then
+     self.x = self.x + (self.speed * dt)
+  end
+
+  if lk.isDown("left") then
+     self.x = self.x - (self.speed * dt)
+  end
+
+  if lk.isDown("down") then
+     self.y = self.y + (self.speed * dt)
+  end
+
+  if lk.isDown("up") then
+     self.y = self.y - (self.speed * dt)
+  end
+end
+
+function Numerobis:draw()
+  lg.draw(self.sprite, self.x, self.y);
+end
