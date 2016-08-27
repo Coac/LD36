5e75e46034714e18fd370a804656d5f30abc171e
Maltiize <etienne.mart@laposte.net>
2016-08-27 15:23:48 +0200

    globals +  enemy

--- scripts/enemy.lua
+++ scripts/enemy.lua
@@ -2,8 +2,21 @@ require("scripts/globals")
 
 local enemy = {}
 
+function Enemy:new(_sprite, _x, _y, _speedY)
+  o = {
+    sprite = _sprite,
+    x = _x,
+    y = _y,
+    speedY =  _speedY
+   }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+
 function enemy.load ()
-  local trainImage = love.graphics.newImage(imagesFolder .. 'enemy')
+  local trainImage = love.graphics.newImage(imagesFolder .. 'enemy.png')
 end
 
 return enemy
