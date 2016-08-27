87e5326eb269715b525b9c0f3bece2773a7eb165
Coac <levictor3000@hotmail.fr>
2016-08-27 13:00:16 +0200

    Graphics test

--- main.lua
+++ main.lua
@@ -1,3 +1,16 @@
+local lg=love.graphics
+
+function love.load()
+  testImg = lg.newImage("assets/test.png")
+end
+
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+  lg.print("Hello World", 400, 300)
+  lg.setNewFont(20)
+  canvas = lg.newCanvas()
+  lg.setLineWidth(50)
+  lg.rectangle('fill', 0, 0, 40, 40)
+  lg.draw(canvas, 0, 0, 0)
+  lg.draw(testImg, 50, 50)
+
 end
