75efbe6e37823d54f2158bfdffe515e6bea520af
Maltiize <etienne.mart@laposte.net>
2016-08-27 15:32:31 +0200

    angle on enemy

--- scripts/enemy.lua
+++ scripts/enemy.lua
@@ -2,12 +2,13 @@ require("scripts/globals")
 
 local Enemy = {}
 
-function Enemy:new(_sprite, _x, _y, _speedY)
+function Enemy:new(_sprite, _x, _y, _speedY,_angle)
   o = {
     sprite = _sprite,
     x = _x,
     y = _y,
     speedY =  _speedY
+    angle =  _angle
    }
   setmetatable(o, self)
   self.__index = self
