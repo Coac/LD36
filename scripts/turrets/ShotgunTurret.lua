4042d15faaa217e70547eabf30f8e287586e3460
Coac <levictor3000@hotmail.fr>
2016-08-28 20:17:27 +0200

    Shotgun turret take good sprite

--- scripts/turrets/ShotgunTurret.lua
+++ scripts/turrets/ShotgunTurret.lua
@@ -3,6 +3,15 @@ require("scripts/Globals")
 
 ShotgunTurret = Turret:new(-999,-999)
 
+local turretSprite = lg.newImage(IMG_DIR .. "turret2.png")
+
+function ShotgunTurret:new(_x, _y)
+  o = Turret:new(_x, _y)
+  o.sprite = turretSprite
+  setmetatable(o, self)
+  self.__index = self
+end
+
 function ShotgunTurret:fire ()
   Arrow:new(self.x, self.y, 1000, self.angle + math.degToRad(15))
   Arrow:new(self.x, self.y, 1000, self.angle)
